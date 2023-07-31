be5eba2745c8e994e0ea61e529375e1c21c78faa
Sean Reeise <35837533+sreeise@users.noreply.github.com>
2023-07-31 00:30:39 -0400

    Resource server minimal example

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 * Use in conjunction with Spring Security Authorization Server.
 * [See the authorization server example that works with this resource server.](https://github.com/sreeise/opaque-authorization-server)
 
+Get the bearer token using the authorization server. Use the bearer token in the request to the messages endpoints:
+
+        export TOKEN="token"
+
+        curl --location --request GET 'http://localhost:8080/message' \
+            --header 'Authorization: Bearer $TOKEN'
--- src/main/java/com/example/resourceserver/security/OAuth2ResourceServerSecurityConfiguration.java
+++ src/main/java/com/example/resourceserver/security/OAuth2ResourceServerSecurityConfiguration.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -17,12 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 @EnableMethodSecurity
 public class OAuth2ResourceServerSecurityConfiguration {
-
-    /*
-    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
-    String jwkSetUri;
-     */
-
     @Value("${spring.security.oauth2.resourceserver.opaque.introspection-uri}")
     String introspectionUri;
 
@@ -38,70 +31,17 @@ public class OAuth2ResourceServerSecurityConfiguration {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/**/message/**").hasAuthority("SCOPE_message:read")
+                        .requestMatchers("/**/message/**").hasAuthority("SCOPE_message:write")
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(configurer ->
-                        configurer.jwt(Customizer.withDefaults())
-                        .opaqueToken((opaqueToken) ->
+                        configurer.opaqueToken((opaqueToken) ->
                                 opaqueToken
                                         .introspectionUri(this.introspectionUri)
                                         .introspectionClientCredentials(this.clientId, this.clientSecret)
                         )
                 );
-         // .oauth2ResourceServer((oauth2) -> oauth2
-        //                        .authenticationManagerResolver(authenticationManagerResolver)
-        //                );
 
         return http.build();
     }
-
-    /*
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-    }
-     */
-
-
-    /*
-        @Bean
-    AuthenticationManagerResolver<HttpServletRequest> authenticationManager(JwtDecoder jwtDecoder,
-                                                                            OpaqueTokenIntrospector opaqueTokenIntrospector) {
-        Map<String, AuthenticationManager> authenticationManagers = new HashMap<>();
-        authenticationManagers.put("login-client", jwt(jwtDecoder));
-        authenticationManagers.put("messaging-client", opaque(opaqueTokenIntrospector));
-        authenticationManagers.put("username-password-client", usernamePasswordAuthorizationManager());
-        return (request) -> {
-            log.debug(request.getRequestURI());
-            log.debug(request.getAuthType());
-            log.debug(request.getMethod());
-            String[] pathParts = request.getRequestURI().split("/");
-            String tenantId = (pathParts.length > 0) ? pathParts[1] : null;
-
-            return Optional.ofNullable(tenantId)
-                    .map(authenticationManagers::get)
-                    .orElseThrow(() -> new IllegalArgumentException("unknown tenant"));
-        };
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
-    }
-
-
-    AuthenticationManager usernamePasswordAuthorizationManager() {
-        return new ProviderManager(new UserAuthenticationProvider());
-    }
-
-    AuthenticationManager jwt(JwtDecoder jwtDecoder) {
-        JwtAuthenticationProvider authenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
-        authenticationProvider.setJwtAuthenticationConverter(new JwtBearerTokenAuthenticationConverter());
-        return new ProviderManager(authenticationProvider);
-    }
-
-    AuthenticationManager opaque(OpaqueTokenIntrospector introspectionClient) {
-        return new ProviderManager(new OpaqueTokenAuthenticationProvider(introspectionClient));
-    }
-     */
 }
